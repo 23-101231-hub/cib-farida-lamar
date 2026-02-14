90e86e94a741454a966875b57dc052bdc3b7dff9
Farida <23-101231@students.eui.edu.eg>
2026-02-14 19:22:09 +0200

    lastcard

--- src/components/Card6.jsx
+++ src/components/Card6.jsx
@@ -5,11 +5,13 @@ import img8 from "../assets/Icon [Layer_1].png"
 const Card6 = () => {
     return (     <div className="wecare-card">
          <img src={img8} />
+         <div className='div'>
         <h3>Online</h3>
         <p>
           Chat with Zaki or our digital assistant to guide you through
           our products and services, or reach out on social media.
         </p>
+        </div>
       </div> );
 }
  
